d7a35ff760f0f5b56a3571710466dd2515cf8974
vanessapigwin <72374043+vanessapigwin@users.noreply.github.com>
2024-08-21 04:46:23 +0800

    update api tests

--- tests/e2e/test_api.py
+++ tests/e2e/test_api.py
@@ -1,6 +1,6 @@
 import pytest
 import requests
-from api_client import post_to_add_batch
+import api_client
 from allocation import config
 
 
@@ -9,22 +9,23 @@ from conftest import random_batchref, random_orderid, random_sku
 
 @pytest.mark.usefixtures("postgres_db")
 @pytest.mark.usefixtures("restart_api")
-def test_happy_path_returns_201_and_allocated_batch():
+def test_happy_path_returns_202_and_allocated_batch():
+    orderid = random_orderid()
     sku, other_sku = random_sku(), random_sku("other")
     earlybatch = random_batchref(1)
     laterbatch = random_batchref(2)
     otherbatch = random_batchref(3)
 
-    post_to_add_batch(laterbatch, sku, 100, "2023-01-01")
-    post_to_add_batch(earlybatch, sku, 100, "2020-01-01")
-    post_to_add_batch(otherbatch, other_sku, 100, None)
-    data = {"orderid": random_orderid(), "sku": sku, "qty": 3}
+    api_client.post_to_add_batch(laterbatch, sku, 100, "2023-01-01")
+    api_client.post_to_add_batch(earlybatch, sku, 100, "2020-01-01")
+    api_client.post_to_add_batch(otherbatch, other_sku, 100, None)
 
-    url = config.get_api_url()
-    r = requests.post(f"{url}/allocate", json=data)
+    r = api_client.post_to_allocate(orderid, sku, qty=3)
+    assert r.status_code == 202
 
-    assert r.status_code == 201
-    assert r.json()["batchref"] == earlybatch
+    r = api_client.get_allocation(orderid)
+    assert r.ok
+    assert r.json() == [{"sku": sku, "batchref": earlybatch}]
 
 
 @pytest.mark.usefixtures("postgres_db")
@@ -32,9 +33,9 @@ def test_happy_path_returns_201_and_allocated_batch():
 def test_unhappy_path_returns_400_and_error_message():
     unknown_sku, orderid = random_sku(), random_orderid()
 
-    data = {"orderid": orderid, "sku": unknown_sku, "qty": 20}
-    url = config.get_api_url()
-    r = requests.post(f"{url}/allocate", json=data)
-
+    r = api_client.post_to_allocate(orderid, unknown_sku, 20, expect_success=False)
     assert r.status_code == 400
     assert r.json()["messages"] == f"Invalid sku: {unknown_sku}"
+
+    r = api_client.get_allocation(orderid)
+    assert r.status_code == 404

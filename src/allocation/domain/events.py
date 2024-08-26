a121f761fef32d8396855682f76f7cd0008a6b58
vanessapigwin <72374043+vanessapigwin@users.noreply.github.com>
2024-08-27 03:27:29 +0800

    add a deallocate event

--- src/allocation/domain/events.py
+++ src/allocation/domain/events.py
@@ -1,6 +1,4 @@
 from dataclasses import dataclass
-from typing import Optional
-from datetime import date
 
 
 class Event:
@@ -18,3 +16,10 @@ class Allocated(Event):
     sku: str
     qty: int
     batchref: str
+
+
+@dataclass
+class Deallocated(Event):
+    orderid: str
+    sku: str
+    qty: int
